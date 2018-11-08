804611205d4b62216dd3344519fb6737593819a5
Javier Gonzalez <jgonzalez@dparadig.com>
2018-11-07 23:52:33 -0300

    init

---
+++ src/main/java/apirest/hello/Greeting.java
@@ -0,0 +1,17 @@
+package apirest.hello;
+
+public class Greeting {
+	private String content;
+
+	public String getContent() {
+		return content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	public Greeting(String content) {
+		setContent(content);
+	}
+}
